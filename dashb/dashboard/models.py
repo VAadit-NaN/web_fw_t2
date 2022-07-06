ff14756098f49dc1a0e1e2d606e8ca57fedaf4b3
VAadit-NaN <aaditya.vikram01@gmail.com>
2022-07-06 16:16:42 +0530

    pycache is my undoing

--- dashb/dashboard/models.py
+++ dashb/dashboard/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class CountryData(models.Model):
+    country = models.CharField(max_length=100)
+    population = models.IntegerField()
+
+    def __str__(self) -> str:
+        #custom format string representation
+        return f'{self.country} -{self.population}'
